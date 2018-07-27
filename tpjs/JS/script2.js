2ce3596c2c98c1999c35f49740b42e5abb75aa7d
Sith Ron <sithigna@gmail.com>
2018-07-27 12:36:00 +0200

    Tp JS 2

--- tpjs/JS/script2.js
+++ tpjs/JS/script2.js
@@ -1,8 +1,31 @@
-function calculerAddition()
+function calculerOperation(pOperation)
 	{
+		var a = document.querySelector("#a").value
+		//(document.getElementById("a")).value; 
+		var b = document.querySelector("#b").value 
+		//(document.getElementById("b")).value;
+		
+		var res = 0;
+		if (pOperation == '+')
+			{
+				res = Number(a) + Number(b); //conversion en number
+			}
+		else if (pOperation == '*')
+			{
+				res = a * b;
+			}
+		document.querySelector("#spanResultat").innerHTML = res;
+	}
 
-		var a = (document.getElementById("a")).value;
-		var b = (document.getElementById("b")).value;
-		var res = Number(a) + Number(b);
-		document.getElementById("spanResultat").innerHTML = res;
+function calculerMultiplication()
+	{
+		calculerOperation("*");
+	}
+
+function initComportementJs()
+	{
+		var btnMultiplication = document.querySelector("#btnMultiplication")
+		//on enregistre le déclement de la fonction calculerMultiplication()
+		btnMultiplication.addEventListener("click", calculerMultiplication, false)
+		//en l'associant à l'évenement "click" sur la zone dont l'id est btnMultiplication
 	}
