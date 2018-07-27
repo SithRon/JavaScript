d48bf5b516cec21bfed3dc48fe3fd57ee2b8fd01
Sith Ron <sithigna@gmail.com>
2018-07-27 16:55:19 +0200

    Tp JS 3

--- tpjs/JS/script3.js
+++ tpjs/JS/script3.js
@@ -4,23 +4,40 @@ function initComportementJs()
 		btnEnregistrer.addEventListener("click", enregistrer, false);
 	}
 
+//function constructeur pour type d'objet "Client"
 function Client(nom, prenom)
 	{
 		this.nom = nom;
 		this.prenom = prenom;
 	}
 
+//déclaration d'un tableau 
+var tabClients = []; 
+
 function enregistrer()
 	{
-		var client = new Client(nom, prenom)
-		client.nom = document.querySelector("#nom").value; 
-		client.prenom = document.querySelector("#prenom").value;
+		var nom = document.querySelector("#nom").value; 
+		var prenom = document.querySelector("#prenom").value;
+		
+		//var client = new Client(nom, prenom)//création d'une instance Client
+		var client = 
+		{
+			nom : nom,
+			prenom : prenom,
+		}; //expression "objet littéral javascript" (très proche du format JSON)
+		//peut remplacer le constructeur "Client"
+
+		//on peut dynamiquement ajouter des propriétés supplémentaires sur l'objet client :
 		client.telephone = document.querySelector("#telephone").value;
 		client.adresse = document.querySelector("#adresse").value;
 		client.email = document.querySelector("#email").value;
-
+		//JSON.stringify() est une fonction prédéfinie par javascript qui construit
+		//automatiquement une chaine de caractère au format JSON à partir de 
+		// toutes les valeurs d'un objet javascript, ça ressemble un peu au
+		//.toString() de java
 		var clientAsJson = JSON.stringify(client);
+		tabClients.push(client); // ajout var client dans tabClients
+
 		document.querySelector("#spanResultat").innerHTML = clientAsJson;
+		document.querySelector("#spanTableau").innerHTML = JSON.stringify(tabClients);
 	}
-
-
